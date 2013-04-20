5d6f9e48e365a8e7c4ad707e04983830b6918fe1
gamesun <gamesunyt@gmail.com>
2013-04-20 22:34:34 +0800

    auto hide when at the side of screen

--- MyDock/MyDockDlg.cpp
+++ MyDock/MyDockDlg.cpp
@@ -31,6 +31,9 @@ void CMyDockDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CMyDockDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_MOUSEMOVE()
+	ON_WM_NCMOUSEMOVE()
+	ON_WM_TIMER()
 END_MESSAGE_MAP()
 
 
@@ -46,6 +49,12 @@ BOOL CMyDockDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
+	m_screenX = GetSystemMetrics( SM_CXSCREEN );
+	m_screenY = GetSystemMetrics( SM_CYSCREEN );
+	m_dwLastActiveTime = GetTickCount();
+	m_enHidePosi = NO;
+	SetTimer( 100, 100, NULL );
+
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
@@ -86,3 +95,142 @@ HCURSOR CMyDockDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CMyDockDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	m_dwLastActiveTime = GetTickCount();
+
+	CDialogEx::OnMouseMove(nFlags, point);
+}
+
+
+void CMyDockDlg::OnNcMouseMove(UINT nHitTest, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	m_dwLastActiveTime = GetTickCount();
+	CDialogEx::OnNcMouseMove(nHitTest, point);
+}
+
+BOOL CMyDockDlg::IsMouseInWindow( void ){
+	CRect rect;
+	GetWindowRect(&rect);
+	CPoint point;
+	GetCursorPos(&point);
+	return rect.PtInRect(point);
+}
+
+void CMyDockDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	switch ( nIDEvent ){
+	case 100:
+		if ( GetTickCount() - m_dwLastActiveTime < 300 ){
+			DockedShow();
+		} else {
+			DockedHidden();
+		}
+		break;
+	}
+
+	CDialogEx::OnTimer(nIDEvent);
+}
+
+#define SEQ_NUM		20
+#define EVERY_TIME	( 60 / SEQ_NUM )
+
+void CMyDockDlg::DockedShow( void ){
+	
+	if( m_enHidePosi != NO && IsMouseInWindow() ){//已隐藏，显示旧值
+		//恢复样式
+		ModifyStyle( NULL, WS_SYSMENU | WS_THICKFRAME );
+		//还原大小
+		int seq = 0;
+		switch( m_enHidePosi ){
+		case TOP:
+			while ( ++seq <= SEQ_NUM ){
+				this->SetWindowPos( NULL, m_rect.left, m_rect.top, m_rect.right - m_rect.left, ( m_rect.bottom - m_rect.top ) * seq / SEQ_NUM, SWP_NOCOPYBITS );
+			    Sleep( EVERY_TIME );
+			}
+			break;
+		case LEFT:
+			while ( ++seq <= SEQ_NUM ){
+				this->SetWindowPos( NULL, m_rect.left, m_rect.top, ( m_rect.right - m_rect.left ) * seq / SEQ_NUM, m_rect.bottom - m_rect.top, SWP_NOCOPYBITS );
+				Sleep( EVERY_TIME );		
+			}
+			break;
+		case RIGHT:
+			while ( ++seq <= SEQ_NUM ){
+				this->SetWindowPos( NULL, m_rect.left + ( m_rect.right - m_rect.left ) * ( SEQ_NUM - seq ) / SEQ_NUM, m_rect.top, m_rect.right - m_rect.left, m_rect.bottom - m_rect.top, SWP_NOCOPYBITS );
+				Sleep( EVERY_TIME );
+			}
+			break;
+		}
+		m_enHidePosi = NO;
+	}
+}
+
+void CMyDockDlg::DockedHidden( void ){
+	CRect rect;
+	GetWindowRect( &rect );
+	
+	if( m_enHidePosi ==NO && !IsMouseInWindow() ){
+		m_rect = rect;
+		if ( m_rect.top <=0 ){//靠顶
+			m_enHidePosi = TOP;
+			ModifyStyle( WS_THICKFRAME | WS_SYSMENU, NULL );
+			this->SetWindowPos( NULL, m_rect.left, 0, m_rect.right - m_rect.left, 2, SWP_NOCOPYBITS );
+			//下移
+			//右移动
+			m_rect.bottom -= m_rect.top;
+			m_rect.top    =  0;
+			
+		} else if ( m_rect.left <=0 ){ //靠左
+			m_enHidePosi = LEFT;
+			ModifyStyle( WS_THICKFRAME | WS_SYSMENU, NULL );
+			this->SetWindowPos( NULL, 0, m_rect.top, 2, m_rect.bottom - m_rect.top, SWP_NOCOPYBITS );
+			//右移动
+			m_rect.right -= m_rect.left;
+			m_rect.left  = 0;
+			
+		} else if ( m_rect.right >= m_screenX ){ //靠右
+			m_enHidePosi = RIGHT;
+			ModifyStyle( WS_THICKFRAME | WS_SYSMENU, NULL );
+			this->SetWindowPos( NULL, m_screenX-2, m_rect.top, 2, m_rect.bottom-m_rect.top, SWP_NOCOPYBITS );
+			//左移动
+			m_rect.left  = m_screenX - ( m_rect.right - m_rect.left );
+			m_rect.right = m_screenX;//m_rect.left; //偏移量   m_rect.right - m_screenX
+			
+		} else {
+			m_enHidePosi = NO;
+		}	
+	}
+}
+
+
+
+void CMyDockDlg::OnOK()
+{
+	// TODO: 在此添加专用代码和/或调用基类
+
+	//CDialogEx::OnOK();
+}
+
+
+void CMyDockDlg::OnCancel()
+{
+	// TODO: 在此添加专用代码和/或调用基类
+
+	CDialogEx::OnCancel();
+}
+
+BOOL CMyDockDlg::PreTranslateMessage(MSG* pMsg)
+{
+	// TODO: 在此添加专用代码和/或调用基类
+	if ( pMsg->message == WM_KEYDOWN && pMsg->wParam == VK_ESCAPE ){
+		//m_obj.SetFocus();
+		return TRUE;
+	}
+	return CDialogEx::PreTranslateMessage(pMsg);
+}
--- MyDock/MyDockDlg.h
+++ MyDock/MyDockDlg.h
@@ -4,6 +4,12 @@
 
 #pragma once
 
+typedef enum _hide_posi{
+	NO,  //非靠边
+	LEFT, //靠左
+	RIGHT,//靠右
+	TOP	  //靠上
+} enHidePosi;
 
 // CMyDockDlg 对话框
 class CMyDockDlg : public CDialogEx
@@ -12,6 +18,9 @@ class CMyDockDlg : public CDialogEx
 public:
 	CMyDockDlg(CWnd* pParent = NULL);	// 标准构造函数
 
+	BOOL IsMouseInWindow( void );
+	void DockedShow( void );
+	void DockedHidden( void );
 // 对话框数据
 	enum { IDD = IDD_MYDOCK_DIALOG };
 
@@ -22,10 +31,21 @@ public:
 // 实现
 protected:
 	HICON m_hIcon;
-
+	int m_screenX;
+	int m_screenY;
+	enHidePosi m_enHidePosi;
+	DWORD m_dwLastActiveTime;
+	CRect m_rect;
 	// 生成的消息映射函数
 	virtual BOOL OnInitDialog();
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnNcMouseMove(UINT nHitTest, CPoint point);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	virtual void OnOK();
+	virtual void OnCancel();
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
 };
